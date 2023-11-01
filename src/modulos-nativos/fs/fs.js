6ec0434194c3946848e8e6d78126a26dceb53951
Reisel Valle <reiselvalle@gmail.com>
2023-11-01 12:32:35 -0500

    readFileSync

--- src/modulos-nativos/fs/fs.js
+++ src/modulos-nativos/fs/fs.js
@@ -1,9 +1,20 @@
 const fs = require('fs');
 
+// readFile
 fs.readFile('archivo.txt', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log(data);
+  console.log('readFile Async >> ',data);
 });
+
+// readFileSync
+try {
+  const data = fs.readFileSync('archivo.txt', 'utf8');
+  console.log('readFile Sync >> ', data);
+} catch (err) {
+  console.error(err);
+}
+
+
